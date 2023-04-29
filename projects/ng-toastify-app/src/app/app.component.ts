4e4df958db09c18d4d9d1c712a5463a56771c58e
HP-PAV-15\bhang <bhangalekunal2631996@gmail.com>
2023-04-29 22:37:01 +0530

    added toaster logic

--- projects/ng-toastify-app/src/app/app.component.ts
+++ projects/ng-toastify-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgToastifyService } from 'ng-toastify';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-toastify-app';
+
+  constructor(private toastService: NgToastifyService) {
+  }
+
+
+  onPrimary(): void { 
+    this.toastService.primary('Your changes saved successfully');
+  }
+
+  onSecondary(): void { 
+    this.toastService.secondary('Your changes saved successfully');
+  }
+
+  onAccent(): void { 
+    this.toastService.accent('Your changes saved successfully');
+  }
+
+  onNeutral(): void { 
+    this.toastService.neutral('Your changes saved successfully');
+  }
+
+  onSuccess(): void { 
+    this.toastService.success('Your changes saved successfully');
+  }
+
+  onError(): void {
+    this.toastService.error('Error has occured while saving changes',{horizontalPosition:'left',verticalPosition:'top'});
+  }
+
+  onWarning(): void {
+    this.toastService.warning('Username you have entered is invalid');
+  }
+
+  onInfo(): void {
+    this.toastService.info('New settings available on your account');
+  }
 }
--- projects/ng-toastify/src/lib/ng-toastify.component.ts
+++ projects/ng-toastify/src/lib/ng-toastify.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgToastifyConfig } from './ng-toastify-config.model';
+import { NgToastifyService } from './ng-toastify.service';
+
+const toastTypeMap = new Map<string, {typeClass: string, icon: string}>([
+  ['toast-primary', { typeClass: 'primary', icon: 'fa-solid fa-circle-exclamation' }],
+  ['toast-secondary', { typeClass: 'secondary', icon: 'fa-solid fa-circle-question' }],
+  ['toast-accent', { typeClass: 'accent', icon: 'fa-solid fa-star' }],
+  ['toast-neutral', { typeClass: 'neutral', icon: 'fa-solid fa-circle-info' }],
+  ['toast-success', { typeClass: 'success', icon: 'fa-solid fa-circle-check' }],
+  ['toast-error', { typeClass: 'error', icon: 'fa-solid fa-circle-xmark' }],
+  ['toast-warning', { typeClass: 'warning', icon: 'fa-solid fa-triangle-exclamation' }],
+  ['toast-info', { typeClass: 'info', icon: 'fa-solid fa-circle-info' }],
+]);
+
 
 @Component({
-  selector: 'lib-ng-toastify',
-  template: `
-    <p>
-      ng-toastify works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'ng-toastify',
+  templateUrl: './ng-toastify.component.html',
+  styleUrls: ['./ng-toastify.component.scss']
 })
 export class NgToastifyComponent {
+  toastConfig: NgToastifyConfig;
+
+  @Output() close = new EventEmitter<void>();
+  
+  constructor(private toastService: NgToastifyService) {
+
+  }
+  
+  getPositonClasses(): any {
+    return {
+      [`${this.toastConfig.verticalPosition}-${this.toastConfig.horizontalPosition}`]: true
+    };
+  }
+
+  getToastTypeClasses(): any {
+    return {
+      [`${toastTypeMap.get(this.toastConfig.type).typeClass}`]: true
+    };
+  }
+
+  getToastIconClasses(): any {
+    return {
+      [`${toastTypeMap.get(this.toastConfig.type).icon}`]: true
+    };
+  }
+  closeToast(): void {
+    this.close.emit();
+  }
 
 }
